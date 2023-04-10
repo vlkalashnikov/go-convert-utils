8ffed3a1d6bd53d086cdf0b7814dad6cced02e8b
v.kalashnikov <vlkalashnikov91@gmail.com>
2023-04-10 23:14:10 +0600

    add SameIntSlice

---
+++ numerics.go
@@ -0,0 +1,21 @@
+package utils
+
+func SameIntSlice(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	diff := make(map[int]int, len(x))
+	for _, _x := range x {
+		diff[_x]++
+	}
+	for _, _y := range y {
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y] -= 1
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}
--- strings.go
+++ strings.go
@@ -19,3 +19,23 @@ func OnlyDigits(s string) bool {
 	_, err := strconv.Atoi(s)
 	return err == nil
 }
+
+func SameStringSlice(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	diff := make(map[string]int, len(x))
+	for _, _x := range x {
+		diff[_x]++
+	}
+	for _, _y := range y {
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y] -= 1
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}
