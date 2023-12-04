056e41d82b578cd0a474c2a1863f03c9d95a1fce
v.kalashnikov <vlkalashnikov91@gmail.com>
2023-12-04 15:15:12 +0600

    new methods

--- days.go
+++ days.go
@@ -190,3 +190,11 @@ func ValidDate(dateStr time.Time) bool {
 	}
 	return true
 }
+
+func TruncateToDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func EndOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 0, t.Location())
+}
